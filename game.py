79b30296ec6429c86d33dce7e5b583e96f1ce1ac
Madhesh <waranmadhesh24@gmail.com>
2026-01-09 16:39:18 +0530

    msg1

--- game.py
+++ game.py
@@ -12,4 +12,18 @@ while running:
 
 pygame.quit()
 print("hello")
+print("hello")import pygame
+pygame.init()
+
+screen = pygame.display.set_mode((500, 400))
+pygame.display.set_caption("Pygame Test")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
+print("hello")
 print("hello")
